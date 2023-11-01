07e929173a523d2133f7dcc33ead7ab8a839b809
joaogbn001 <103936808+joaogbn001@users.noreply.github.com>
2023-11-01 14:06:11 -0300

    01/10

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-alert( 'boa tarde.');
-doucment.querySelector('.tecla_pom')
+function tocaSomPom(){
+document.querySelector('#som_tecla_pom').play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador<listaDeTeclas.length){
+   
+    listaDeTeclas[0].onclick = tocaSomPom;
+    contador = contador + 1;
+
+
+    console.log(contador);
+
+}
