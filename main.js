980756e10ddf2c1c4c77e96e93fb4c2fef0dfdc8
joaogbn001 <103936808+joaogbn001@users.noreply.github.com>
2023-11-29 14:32:07 -0300

    29/11

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ document.querySelector('#som_tecla_pom').play();
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-
+//enquanto
 while(contador<listaDeTeclas.length){
+const instrumento = listasdeteclas[contador].classlist[1];
+
+    console.log(instrumento);
    
     listaDeTeclas[0].onclick = tocaSomPom;
     contador = contador + 1;
